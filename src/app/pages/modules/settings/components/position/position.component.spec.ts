e3ead0c3d1da10fb81ee84fd82dbea7af0ccad94
Leonty Chudinov <leonty.chudinov@gmail.com>
2022-10-09 11:19:22 +0500

    Fix tests for Positions Component
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

--- src/app/pages/modules/settings/components/position/position.component.spec.ts
+++ src/app/pages/modules/settings/components/position/position.component.spec.ts
@@ -1,3 +1,4 @@
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
@@ -17,6 +18,7 @@ describe('PositionComponent', () => {
       imports: [
         MaterialModule,
         RouterTestingModule,
+        NoopAnimationsModule,
       ],
       providers: [
         {
