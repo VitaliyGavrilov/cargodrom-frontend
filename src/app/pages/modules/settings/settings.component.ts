58e46a3cf005edddc0b820f3fc29baf18f02046a
Leonty Chudinov <leonty.chudinov@gmail.com>
2022-10-09 20:30:22 +0500

    Make Settings Menu work
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

--- src/app/pages/modules/settings/settings.component.ts
+++ src/app/pages/modules/settings/settings.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+interface MenuGroup {
+  title: string;
+  cssClass: string;
+  expanded: boolean;
+  items: MenuItem[];
+}
+
+interface MenuItem {
+  title: string;
+  link: string;
+  canAdd?: boolean;
+  addButtonTitle?: string;
+}
 
 @Component({
   selector: 'app-settings',
@@ -6,11 +22,90 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./settings.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  settings: MenuGroup[] = [
+    {
+      title: 'Общие настройки',
+      cssClass: 'settings-common',
+      expanded: true,
+      items: [
+        {
+          title: 'Личные настройки',
+          link: './personal'
+        },
+        {
+          title: 'Организации',
+          link: './company',
+          canAdd: true,
+          addButtonTitle: 'Добавить организацию'
+        }
+      ]
+    },
+    {
+      title: 'Структура организации',
+      cssClass: 'settings-company-structure',
+      expanded: true,
+      items: [
+        {
+          title: 'Подразделения',
+          link: './department',
+          canAdd: true,
+          addButtonTitle: 'Добавить подразделение'
+        },
+        {
+          title: 'Должности',
+          link: './position',
+          canAdd: true,
+          addButtonTitle: 'Добавить должность'
+        },
+        {
+          title: 'Сотрудники',
+          link: './employee',
+          canAdd: true,
+          addButtonTitle: 'Добавить сотрудника'
+        }
+      ]
+    },
+  ];
+  activeMenuItem?: MenuItem;
+  activeMenuGroup?: MenuGroup;
+  routerEventSubscription: Subscription;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.routerEventSubscription = router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        this.detectMenuItemAndGroup();
+      }
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.routerEventSubscription.unsubscribe();
+  }
+
+  toggleGroup(group: MenuGroup): void {
+    group.expanded = !group.expanded;
+  }
+
+  private detectMenuItemAndGroup(): void {
+    for (const group of this.settings) {
+      for (const item of group.items) {
+        const urlTree = this.router.createUrlTree([item.link], { relativeTo: this.activatedRoute, });
+        const isActive = this.router.isActive(urlTree, { paths: 'subset', matrixParams: 'ignored', queryParams: 'ignored', fragment: 'ignored' });
+        if (isActive) {
+          this.activeMenuItem = item;
+          this.activeMenuGroup = group;
+          return;
+        }
+      }
+    }
   }
 
 }
