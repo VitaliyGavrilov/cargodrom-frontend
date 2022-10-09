640299c037d8f54feb31f201fbc60f53678d1434
Leonty Chudinov <leonty.chudinov@gmail.com>
2022-10-09 12:07:08 +0500

    Add Employee component
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

--- src/app/pages/modules/settings/components/employee/employee.component.spec.ts
+++ src/app/pages/modules/settings/components/employee/employee.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from '@cargodrom/material/material.module';
+import { of } from 'rxjs';
+import { CompanyService } from 'src/app/api/services/company.service';
 
 import { EmployeeComponent } from './employee.component';
 
@@ -8,7 +13,19 @@ describe('EmployeeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EmployeeComponent ]
+      declarations: [ EmployeeComponent ],
+      imports: [
+        MaterialModule,
+        RouterTestingModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: CompanyService, useValue: {
+            companyEmployeeList: () => of([])
+          }
+        },
+      ]
     })
     .compileComponents();
 
--- src/app/pages/modules/settings/components/employee/employee.component.ts
+++ src/app/pages/modules/settings/components/employee/employee.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { CompanyService } from './../../../../../api/services/company.service';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: [
+    './employee.component.scss',
+    '../../main-table.scss'
+  ],
+  encapsulation: ViewEncapsulation.None,
 })
 export class EmployeeComponent implements OnInit {
+  employees: any;
+  total = 0;
+  start = 0;
+  limits = [25, 50, 100];
+  count = this.limits[0];
 
-  constructor() { }
+  constructor(
+    private companyService: CompanyService,
+  ) { }
 
   ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  loadEmployees(): void {
+    this.companyService.companyEmployeeList().subscribe(employees => {
+      console.log(employees);
+      this.employees = employees ? Object.values(employees) : [];
+      this.total = this.employees.length;
+      console.log(JSON.stringify(employees, null, 2));
+      console.log(JSON.stringify(this.employees, null, 2));
+    });
+  }
+
+  onStartChange(newStart: number): void {
+    this.loadEmployees();
+  }
+
+  onCountChange(newCount: number): void {
+    this.loadEmployees();
   }
 
 }
