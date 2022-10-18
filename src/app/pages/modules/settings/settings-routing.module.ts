9fa847c89666bc4a3411af634a221e9fc130c507
Leonty Chudinov <leonty.chudinov@gmail.com>
2022-10-18 09:02:02 +0500

    Redirect to personal settings by default
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

--- src/app/pages/modules/settings/settings-routing.module.ts
+++ src/app/pages/modules/settings/settings-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'department',
+        redirectTo: 'personal',
         pathMatch: 'full',
       },
       {
