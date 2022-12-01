f228721bbaf5c1ac5b85be2c3b6e156c2e9cff85
Leonty Chudinov <leonty.chudinov@gmail.com>
2022-12-01 14:46:03 +0500

    Create Settings Table base class
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

---
+++ src/app/pages/modules/settings/classes/settings-table.ts
@@ -0,0 +1,157 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { SortColumn } from './../../../../api/custom_models/sort-column';
+import { Directive, OnInit, OnDestroy, ViewChild, TemplateRef } from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { MatSnackBarConfig } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+export interface Column<T> extends SortColumn<T> {
+  title: string;
+}
+
+@Directive()
+export abstract class SettingsTable<T extends {id: number}> implements OnInit, OnDestroy {
+  snackBarWithShortDuration: MatSnackBarConfig = { duration: 1000 };
+  snackBarWithLongDuration: MatSnackBarConfig = { duration: 5000 };
+  protected abstract columns: Column<T>[];
+  protected abstract load<T>(params: { start?: number, count?: number, sort?: SortColumn<T>[] }): Observable<{ total: number, items: T[] }>;
+  protected abstract delete(params: { body: { id: number } }): Observable<void>;
+  
+  protected abstract removedMessage: string;
+
+
+  protected destroy$ = new Subject<void>();
+  rows: T[] = [];
+  total = 0;
+  start = 0;
+  limits = [10, 25, 50, 100];
+  count = this.limits[0];
+  abstract sortCol: keyof T;
+  abstract sortByName: SortColumn<T>;
+  sortDir: 'asc' | 'desc' = 'asc';
+  @ViewChild('removeDialogRef') removeDialogRef!: TemplateRef<T>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar,  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.queryParamMap
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(queryParamMap => {
+        this.start = this.getIntParamSafely(queryParamMap, 'start', this.start);
+        this.count = this.getIntEnumParamSafely(queryParamMap, 'count', this.limits, this.count);
+        this.sortCol = this.getEnumParamSafely(queryParamMap, 'sortCol', this.columns.map(col => col.field) as string[], this.sortCol as string) as keyof T;
+        this.sortDir = this.getEnumParamSafely(queryParamMap, 'sortDir', ['asc', 'desc'], this.sortDir) as 'asc' | 'desc';
+        this.loadRows();
+      });
+  }
+
+  private loadRows(): void {
+    const sortCol: SortColumn<T> = {
+      field: this.sortCol,
+      dir: this.sortDir,
+    };
+
+    const sort = sortCol.field !== this.sortByName.field ? [sortCol, this.sortByName] : [sortCol];
+    this.load({ start: this.start, count: this.count, sort: JSON.stringify(sort) as unknown as SortColumn<T>[] }).subscribe(rows => {
+      this.rows = rows ? rows.items as T[] : [];
+      this.total = rows.total;
+    });
+  }
+
+  getIntParamSafely(queryParamMap: ParamMap, name: string, fallback: number): number {
+    const value = queryParamMap.get(name);
+    if (value != null) {
+      const intValue = parseInt(value, 10);
+      return intValue;
+    }
+    return fallback;
+  }
+
+  getIntEnumParamSafely(queryParamMap: ParamMap, name: string, values: number[], fallback: number): number {
+    const value = queryParamMap.get(name);
+    if (value != null) {
+      const intValue = parseInt(value, 10);
+      return values.includes(intValue) ? intValue : fallback;
+    }
+    return fallback;
+  }
+
+  getEnumParamSafely(queryParamMap: ParamMap, name: string, values: string[], fallback: string): string {
+    const value = queryParamMap.get(name);
+    if (value != null && values.includes(value)) {
+      return value;
+    }
+    return fallback;
+  }
+  
+  onStartChange(newStart: number): void {
+    this.router.navigate(['.'], {
+      queryParams: { start: newStart },
+      queryParamsHandling: 'merge',
+      relativeTo: this.route,
+    });
+  }
+
+  onCountChange(newCount: number): void {
+    this.router.navigate(['.'], {
+      queryParams: { count: newCount, start: 0 },
+      queryParamsHandling: 'merge',
+      relativeTo: this.route,
+    });
+  }
+
+  sort(field: keyof T): void {
+    this.start = 0;
+    if (this.sortCol === field) {
+      this.sortDir = this.sortDir === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortDir = 'asc';
+      this.sortCol = field;
+    }
+    this.router.navigate(['.'], {
+      queryParams: { sortCol: this.sortCol, sortDir: this.sortDir, start: this.start },
+      queryParamsHandling: 'merge',
+      relativeTo: this.route,
+    });
+  }
+  
+  confirmRemove(row: T): void {
+    this.dialog.open(this.removeDialogRef, { data: row }).afterClosed().subscribe(res => {
+      if (res) {
+        this.remove(row);
+      }
+    });
+  }
+
+  remove(row: T): void {
+    const body = { id: row.id };
+    this.delete({ body })
+      .subscribe({
+        next: () => {
+          this.snackBar.open(this.removedMessage, undefined, this.snackBarWithShortDuration);
+          this.loadRows();
+        },
+        error: (err) => this.snackBar.open(`Ошибка удаления: ` + err?.error?.error_message, undefined, { duration: 1000 })
+      });
+  }
+  
+  getColTitle(col: Column<T>): string {
+    if (col.field === this.sortCol) {
+      return col.dir === 'asc' ? 'сортировать по убыванию' : 'сортировать по возрастанию'
+    }
+    return 'сортировать по возрастанию';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+}
--- src/app/pages/modules/settings/components/department/department.component.ts
+++ src/app/pages/modules/settings/components/department/department.component.ts
@@ -1,17 +1,12 @@
-import { takeUntil } from 'rxjs/operators';
 import { SortColumn } from './../../../../../api/custom_models/sort-column';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Department } from './../../../../../api/custom_models/department';
 import { CompanyService } from './../../../../../api/services/company.service';
-import { Component, OnInit, TemplateRef, ViewChild, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Employee } from 'src/app/api/custom_models';
-import { Subject } from 'rxjs';
-
-interface Column<T> extends SortColumn<T> {
-  title: string;
-}
+import { Observable } from 'rxjs';
+import { Column, SettingsTable } from '../../classes/settings-table';
 
 @Component({
   selector: 'app-department',
@@ -22,152 +17,36 @@ interface Column<T> extends SortColumn<T> {
   ],
   encapsulation: ViewEncapsulation.None,
 })
-export class DepartmentComponent implements OnInit, OnDestroy {
-  departments: Department[] = [];
-  total = 0;
-  start = 0;
-  limits = [10, 25, 50, 100];
-  count = this.limits[0];
-  @ViewChild('removeDialogRef') removeDialogRef!: TemplateRef<Department>;
+export class DepartmentComponent extends SettingsTable<Department> {
   columns: Column<Department>[] = [
     { field: 'name', title: 'Название подразделения', dir: 'asc' },
     { field: 'count_position', title: 'Должностей', dir: 'asc' },
     { field: 'count_user', title: 'Сотрудников', dir: 'asc' },
     { field: 'leader_user', title: 'Руководитель подразделения', dir: 'asc' },
   ];
-  sortCol: keyof Department =  this.columns[0].field;
-  sortDir = this.columns[0].dir;
-  employees: Employee[] = [];
-  destroy$ = new Subject<void>();
-
+  sortCol = this.columns[0].field;
+  sortByName: SortColumn<Department> = {
+    dir: 'asc',
+    field: 'name',
+  };
+  removedMessage = `Подразделение удалено`;
 
   constructor(
     private companyService: CompanyService,
-    private route: ActivatedRoute,
-    private dialog: MatDialog,
-    private snackBar: MatSnackBar,
-    private router: Router,
-  ) { }
-
-  ngOnInit(): void {
-    this.route.queryParamMap
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(queryParamMap => {
-      this.start = this.getIntParamSafely(queryParamMap, 'start', this.start);
-      this.count = this.getIntEnumParamSafely(queryParamMap, 'count', this.limits, this.count);
-      this.sortCol = this.getEnumParamSafely(queryParamMap, 'sortCol', this.columns.map(col => col.field), this.sortCol) as keyof Department;
-      this.sortDir = this.getEnumParamSafely(queryParamMap, 'sortDir', ['asc', 'desc'], this.sortDir) as 'asc' | 'desc';
-      this.loadDepartments();
-    });
+    dialog: MatDialog,
+    snackBar: MatSnackBar,
+    route: ActivatedRoute,
+    router: Router,
+  ) {
+    super(route, router, dialog, snackBar);
   }
   
-  getIntParamSafely(queryParamMap: ParamMap, name: string, fallback: number): number {
-    const value = queryParamMap.get(name);
-    if (value != null) {
-      const intValue = parseInt(value, 10);
-      return intValue;
-    }
-    return fallback;
-  }
-
-  getIntEnumParamSafely(queryParamMap: ParamMap, name: string, values: number[], fallback: number): number {
-    const value = queryParamMap.get(name);
-    if (value != null) {
-      const intValue = parseInt(value, 10);
-      return values.includes(intValue) ? intValue : fallback;
-    }
-    return fallback;
-  }
-
-  getEnumParamSafely(queryParamMap: ParamMap, name: string, values: string[], fallback: string): string {
-    const value = queryParamMap.get(name);
-    if (value != null && values.includes(value)) {
-      return value;
-    }
-    return fallback;
+  load<Department>(params: { start?: number; count?: number; sort?: SortColumn<Department>[]; }): Observable<{ total: number; items: Department[]; }> {
+    return this.companyService.companyDepartmentList(params as any) as Observable<{ total: number; items: Department[]; }>;
   }
   
-  loadDepartments(): void {
-    const sortCol: SortColumn<Department> = {
-      field: this.sortCol,
-      dir: this.sortDir,
-    };
-    const sortByName: SortColumn<Department> = {
-      dir: 'asc',
-      field: 'name',
-    };
-    const sort = sortCol.field !== sortByName.field ? [sortCol, sortByName] : [sortCol];
-    this.companyService.companyDepartmentList({start: this.start, count: this.count, sort: JSON.stringify(sort) as unknown as SortColumn<Department>[] }).subscribe(departments => {
-      this.departments = departments ? departments.items as Department[] : [];
-      this.total = departments.total!;
-    });
+  delete(params: { body: { id: number; } }): Observable<void> {
+    return this.companyService.companyDepartmentDelete(params) as unknown as Observable<void>;
   }
 
-  onStartChange(newStart: number): void {
-    this.router.navigate(['.'], {
-      queryParams: { start: newStart },
-      queryParamsHandling: 'merge',
-      relativeTo: this.route,
-    });
-  }
-
-  onCountChange(newCount: number): void {
-    this.router.navigate(['.'], {
-      queryParams: { count: newCount, start: 0 },
-      queryParamsHandling: 'merge',
-      relativeTo: this.route,
-    });
-  }
-  
-  sort(col: Column<Department>): void {
-    this.start = 0;
-    if (this.sortCol === col.field) {
-      this.sortDir = this.sortDir === 'asc' ? 'desc' : 'asc';
-    } else {
-      this.sortDir = 'asc';
-      this.sortCol = col.field;
-    }
-    this.router.navigate(['.'], {
-      queryParams: { sortCol: this.sortCol, sortDir: this.sortDir, start: this.start },
-      queryParamsHandling: 'merge',
-      relativeTo: this.route,
-    });
-  }
-  
-  findColumnByName(name: keyof Department) {
-    return this.columns.find(column => column.field === name);
-  }
-
-  getColTitle(col: Column<Department>): string {
-    if (col.field === this.sortCol) {
-      return col.dir === 'asc' ? 'сортировать по убыванию' : 'сортировать по возрастанию'
-    }
-    return 'сортировать по возрастанию';
-  }
-  
-  confirmRemove(department: Department): void {
-    this.dialog.open(this.removeDialogRef, { data: department }).afterClosed().subscribe(res => {
-      if (res) {
-        this.removeDepartment(department);
-      }
-    });
-  }
-
-  removeDepartment(department: Department): void {
-    const body = { id: department.id };
-    this.companyService.companyDepartmentDelete({ body })
-      .subscribe({
-        next: () => {
-          this.snackBar.open(`Подразделение ${department.name} удалено`, undefined, { duration: 1000 });
-          this.loadDepartments();
-        },
-        error: (err) => this.snackBar.open(`Ошибка удаления подразделения: ` + err.error.error_message, undefined, { duration: 1000 })
-      });
-  }
-  
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-  
 }
