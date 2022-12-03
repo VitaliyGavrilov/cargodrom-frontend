de0b383be5772de6c05ed0861373c705894d7781
Leonty Chudinov <leonty.chudinov@gmail.com>
2022-12-03 10:10:37 +0500

    Sorting for department Employee component
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

--- src/app/pages/modules/settings/components/department-employee/department-employee.component.ts
+++ src/app/pages/modules/settings/components/department-employee/department-employee.component.ts
@@ -2,8 +2,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { Employee } from './../../../../../api/custom_models/employee';
 import { CompanyService } from 'src/app/api/services/company.service';
-import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { Position } from 'src/app/api/custom_models';
+import { Component, Input } from '@angular/core';
+import { SettingsTable } from '../../classes/settings-table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SortColumn } from 'src/app/api/custom_models/sort-column';
 
 @Component({
   selector: 'app-department-employee',
@@ -13,68 +16,33 @@ import { Position } from 'src/app/api/custom_models';
     '../../main-table.scss'
   ]
 })
-export class DepartmentEmployeeComponent implements OnInit {
+export class DepartmentEmployeeComponent extends SettingsTable<Employee> {
   @Input() departmentId!: number;
-  employees: Employee[] = [];
-  positions: Position[] = [];
-  @ViewChild('removeDialogRef') removeDialogRef!: TemplateRef<Employee>;
-
+  removedMessage = `Сотрудник удален`;
+  sortCol = 'name_f' as keyof Employee;
 
   constructor(
     private companyService: CompanyService,
-    private dialog: MatDialog,
-    private snackBar: MatSnackBar,
+    dialog: MatDialog,
+    snackBar: MatSnackBar,
+    route: ActivatedRoute,
+    router: Router,
   ) {
-
+    super(route, router, dialog, snackBar);
   }
 
-  ngOnInit(): void {
-    this.loadEmployeesForDepartment(this.departmentId);
-    this.loadPositions();
+  load<Employee>(params: { start?: number; count?: number; sort?: SortColumn<Employee>[]; }): Observable<{ total: number; items: Employee[]; }> {
+    const queryParams = { ...params, department_id: this.departmentId }
+    return this.companyService.companyEmployeeList(queryParams as any) as unknown as Observable<{ total: number; items: Employee[]; }>;
   }
 
-  loadEmployeesForDepartment(departmentId: number): void {
-    const params = { department_id: departmentId };
-    this.companyService.companyEmployeeList(params)
-      .subscribe(employees => {
-        this.employees = employees ? employees.items as Employee[] : [];
-      })
-  }
-
-  loadPositions(): void {
-    this.companyService.companyPositionList()
-      .subscribe(positions => {
-        this.positions = positions ? positions.items as Position[] : [];
-      })
-  }
-
-  getPositionById(id: number): string | undefined {
-    return this.positions.find(position => position.id === id)?.name;
+  delete(params: { body: { id: number; } }): Observable<void> {
+    return this.companyService.companyEmployeeDelete(params) as unknown as Observable<void>;
   }
 
   onDepartmentLeaderChange(employee: Employee, isLeader: boolean): void {
     const body = {...employee, department_leader: isLeader ? 1: 0} as any;
-    this.companyService.companyEmployeeUpdate({body}).subscribe(() => this.loadEmployeesForDepartment(this.departmentId));
+    this.companyService.companyEmployeeUpdate({body}).subscribe(() => this.loadRows());
   }
 
-  confirmRemove(employee: Employee): void {
-    this.dialog.open(this.removeDialogRef, { data: employee }).afterClosed().subscribe(res => {
-      if (res) {
-        this.removeEmployee(employee);
-      }
-    });
-  }
-
-  removeEmployee(employee: Employee): void {
-    const body = { id: employee.id };
-    this.companyService.companyEmployeeDelete({ body })
-      .subscribe({
-        next: () => {
-          this.snackBar.open(`Сотрудник ${employee.name_f} удалена`, undefined, {duration: 1000});
-          this.loadEmployeesForDepartment(this.departmentId);
-        },
-        error: (err) => this.snackBar.open(`Ошибка удаления сотрудника: ` + err.error.error_message, undefined, {duration: 1000})
-      });
-  }
-
-}
+ }
