e3de0508f88eda708c4982196f3a09d85ab403b8
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-06-02 17:48:34 +0500

    Implement top part of client editor form

--- src/app/pages/components/client-editor/client-editor.component.spec.ts
+++ src/app/pages/components/client-editor/client-editor.component.spec.ts
@@ -8,6 +8,11 @@ import { MatDialog } from '@angular/material/dialog';
 import { CustomerService, SystemService } from 'src/app/api/services';
 import { of } from 'rxjs';
 import { EditorHeaderMockup } from '@cargodrom/material/components/editor-header/editor-header.mockup';
+import { CityService } from '../../services/city.service';
+import { CountryService } from '../../services/country.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ClientEditorComponent', () => {
   let component: ClientEditorComponent;
@@ -19,6 +24,9 @@ describe('ClientEditorComponent', () => {
         FormsModule,
         ReactiveFormsModule,
         RouterTestingModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        NoopAnimationsModule,
       ],
       declarations: [ClientEditorComponent, EditorHeaderMockup],
       providers: [
@@ -28,6 +36,16 @@ describe('ClientEditorComponent', () => {
         {
           provide: CustomerService, useValue: <Partial<CustomerService>>{
             customerInfo: () => of({ id: 1, name: 'hello' })
+          }
+        },
+        {
+          provide: CityService, useValue: <Partial<CityService>>{
+            getCities: () => of([{ id: 1, name: 'hello' }])
+          }
+        },
+        {
+          provide: CountryService, useValue: <Partial<CountryService>>{
+            getCountries: () => of([{ id: 1, name: 'Страна' }])
           }
         },
       ]
--- src/app/pages/components/client-editor/client-editor.component.ts
+++ src/app/pages/components/client-editor/client-editor.component.ts
@@ -1,20 +1,24 @@
+import { innValidator } from './../../../validators/pattern-validator';
 import { Validators } from '@angular/forms';
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Client } from 'src/app/api/custom_models';
+import { Observable, tap } from 'rxjs';
+import { City, Client, Country } from 'src/app/api/custom_models';
 import { CustomerService, SystemService } from 'src/app/api/services';
 import { Editor } from 'src/app/classes/editor';
 import { Location } from '@angular/common';
+import { CityService } from '../../services/city.service';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-client-editor',
   templateUrl: './client-editor.component.html',
-  styleUrls: ['./client-editor.component.scss']
+  styleUrls: ['./client-editor.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class ClientEditorComponent extends Editor<Client> {
+export class ClientEditorComponent extends Editor<Client> implements OnInit{
   private entity = 'Клиент';
   editTitle = 'Информация о клиенте';
   newTitle = 'Добавление клиента';
@@ -22,7 +26,10 @@ export class ClientEditorComponent extends Editor<Client> {
   removedMessage = `${this.entity} удален`;
   createdMessage = `${this.entity} создан`;
   notFoundMessage = `${this.entity} не найден`;
-  
+
+  countries: Country[] = [];
+  cities: City[] = [];
+
   constructor(
     private fb: FormBuilder,
     snackBar: MatSnackBar,
@@ -31,13 +38,26 @@ export class ClientEditorComponent extends Editor<Client> {
     location: Location,
     router: Router,
     private customerService: CustomerService,
+    private cityService: CityService,
+    private countryService: CountryService,
   ) {
     super(location, systemService, route, snackBar, router);
     this.form = this.fb.group({
       id: [''],
       name: ['', [Validators.required]],
       name_full: ['', [Validators.required]],
+      country_id: ['', [Validators.required]],
+      city_id: ['', [Validators.required]],
+      inn: ['', [innValidator]],
+      kpp: ['', []],
+      ogrn: ['', []],
+      okpo: ['', []],
     });
+  }
+  
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this.getCountries();
   }
 
   protected override create(params: { body: Omit<Client, 'id'>; }): Observable<{ id: number; }> {
@@ -45,7 +65,7 @@ export class ClientEditorComponent extends Editor<Client> {
   }
 
   protected override read(params: { id: number; }): Observable<Client> {
-    return this.customerService.customerInfo(params) as Observable<Client>
+    return this.customerService.customerInfo(params).pipe(tap(({country_id}) => this.getCities(country_id!))) as Observable<Client>
   }
 
   protected override update(params: { body: Partial<Client>; }): Observable<void> {
@@ -58,5 +78,20 @@ export class ClientEditorComponent extends Editor<Client> {
 
   protected override getNameForHeader(body: Client): string {
     return `${body.name}`;
+  }
+  
+  private getCountries() {
+    this.countryService.getCountries()
+      .subscribe(countries => this.countries = countries);
+  }
+
+  private getCities(countryId: number) {
+    this.cityService.getCities(countryId)
+      .subscribe(cities => this.cities = cities);
+  }
+
+  onCountryChange(countryId: number): void {
+    this.form.controls['city_id'].reset(undefined);
+    this.getCities(countryId);
   }
 }
