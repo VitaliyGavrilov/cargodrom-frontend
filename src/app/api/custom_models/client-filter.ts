a27cfbe1350076d410d44472be6cf41feb05cf85
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-06-04 11:31:34 +0500

    Begin with Client Filter

--- src/app/api/custom_models/client-filter.ts
+++ src/app/api/custom_models/client-filter.ts
@@ -1,3 +1,25 @@
+import { SortColumn } from "./sort-column";
+import { Client } from "./client";
+
 export interface ClientFilter {
   
+  country_id?: number;
+  group_id: number;
+  inn?: string;
+  contact_id?: number;
+  
+  /**
+   * Начальная позиция
+   */
+  start?: number;
+
+  /**
+   * Лимит позиций на страницу
+   */
+  count?: number;
+
+  /**
+   * Сортировка
+   */
+  sort?: SortColumn<Client>[];
 }
--- src/app/pages/components/client-filter/client-filter.component.spec.ts
+++ src/app/pages/components/client-filter/client-filter.component.spec.ts
@@ -1,6 +1,14 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClientFilterComponent } from './client-filter.component';
+import { CountryService } from '../../services/country.service';
+import { of } from 'rxjs';
+import { CustomerService } from 'src/app/api/services';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ClientFilterComponent', () => {
   let component: ClientFilterComponent;
@@ -8,9 +16,28 @@ describe('ClientFilterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ClientFilterComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        NoopAnimationsModule,
+      ],
+      declarations: [ClientFilterComponent],
+      providers: [
+        {
+          provide: CountryService, useValue: <Partial<CountryService>>{
+            getCountries: () => of([]),
+          }
+        },
+        {
+          provide: CustomerService, useValue: <Partial<CustomerService>>{
+            customerGroupList: () => of([]),
+          }
+        },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ClientFilterComponent);
     component = fixture.componentInstance;
